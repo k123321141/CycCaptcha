bb0a8a7114d2d628b015cf532564d06e0c3cdcc9
k123321141 <kk123321141@gmail.com>
2023-09-24 21:42:18 +0800

    Make it harder

--- src/captcha.py
+++ src/captcha.py
@@ -17,10 +17,12 @@ class SimpleCaptcha(object):
             for n in self._fonts
         ])
 
-    def write(self, chars, output, blur: bool, **kwargs):
+    def write(self, chars, output, blur: bool, verbose: bool, **kwargs):
         im = self.create_captcha_image(chars)
         if blur:
             im = im.filter(ImageFilter.SMOOTH)
+        if verbose:
+            return im
         return im.save(output, format='gif')
 
     @staticmethod
@@ -107,4 +109,207 @@ class SimpleCaptcha(object):
             self.add_dot_noise(image, draw)
 
         image = image.resize((68, 25), PIL.Image.BICUBIC)
+        return image
+
+
+class HardCaptcha(SimpleCaptcha):
+
+    def __init__(self, fonts):
+        super().__init__(fonts)
+        self.chars = ['@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+        self.table = []
+        for i in range(256):
+            self.table.append(i * 1.97)
+
+    @staticmethod
+    def random_gray():
+        r = random.randint(115, 130)
+        g = r + random.randint(-5, 5)
+        b = r + random.randint(-5, 5)
+        return (r, g, b)
+
+    @staticmethod
+    def random_dark_color():
+        r = random.randint(70, 120)
+        g = int(random.gauss(60, 15))
+        b = random.randint(30, 80)
+        return (r, g, b)
+
+    @staticmethod
+    def noised_background(w, h):
+        m = random.randint(115, 185)
+        arr = np.random.normal(m, 40, size=(h, w, 3)).astype(np.uint8)
+        im = Image.fromarray(arr, mode='RGB')
+        return im
+
+    @staticmethod
+    def random_line_color():
+        r = random.randint(35, 70)
+        g = random.randint(35, 70)
+        b = random.randint(35, 70)
+        return (31, 33, 32)
+        return (r, g, b)
+
+    @staticmethod
+    def random_curve_color():
+        r = random.randint(35, 70)
+        g = random.randint(35, 70)
+        b = random.randint(35, 70)
+        return (r, g, b)
+
+    def _generate_char(self, c, factor, color, draw, font):
+        w, h = draw.textsize(c, font=font)
+
+        dx = random.randint(0, 2)
+        dy = random.randint(0, 2)
+        im = Image.new('RGBA', (w + dx, h + dy))
+        Draw(im).text((dx, dy), c, font=font, fill=color)
+
+        # warp
+        dx = w * random.uniform(0.5, 0.9)
+        dy = h * random.uniform(0.6, 0.9)
+        x1 = int(random.uniform(-dx, dx))
+        y1 = int(random.uniform(-dy, dy))
+        x2 = int(random.uniform(-dx, dx))
+        y2 = int(random.uniform(-dy, dy))
+        w2 = w + abs(x1) + abs(x2)
+        h2 = h + abs(y1) + abs(y2)
+        im = im.resize((w2, h2))
+        # im = im.resize((random.randint(250, 350), random.randint(150, 250)))
+        im = im.resize((int(w2*factor), int(h2*factor)), PIL.Image.BILINEAR)
+
+        return im
+
+    def add_char_noise(self, image, c, factor, pos, draw):
+        font = random.choice(self.truefonts)
+        img = self._generate_char(c, factor, self.random_gray(), draw, font)
+        w, h = img.size
+        mask = img.convert('L').point(self.table)
+        image.paste(img, pos, mask)
+
+    def add_line_noise(self, image, draw):
+        color = self.random_line_color()
+        draw.line(
+            [
+                0,
+                random.randint(0, self._height),
+                self._width,
+                random.randint(-self._height * 2, 2 * self._height),
+            ],
+            width=7, fill=color
+        )
+
+    def _generate_curve(self, draw, freq, height):
+        if freq < 1:
+            width = 10
+        elif 3 < freq < 10:
+            width = 6
+        else:
+            width = 3
+
+        im = Image.new('RGBA', (5000, 200))
+        color = self.random_curve_color()
+        r = 70
+        w = width
+#         offset = w//2+3
+        offset = 2
+        for i in range(0, 50):
+
+            Draw(im).arc([i*2*r, 0, (i*2)*r + r, r], 270, 0, width=w, fill=color)
+            Draw(im).arc([(i*2)*r + r - offset, 0, (i+1)*2*r, r], 90, 180, width=w, fill=color)
+            Draw(im).arc([(i*2)*r + r - offset, 0, (i+1)*2*r, r], 0, 90, width=w, fill=color)
+            Draw(im).arc([(i+1)*2*r - offset, 0, (i+1)*2*r+r + offset, r], 180, 270, width=w, fill=color)
+        im = im.crop(im.getbbox())
+        im = im.resize((int(1624*(1+freq)), height), PIL.Image.BICUBIC)
+        return im
+
+    def add_curve_noise(self, image, draw):
+        mode = random.choices(['thin', 'normal', 'heavy', 'flat'], [0.25, 0.25, 0.25, 0.25], k=1)[0]
+        if mode == 'thin':
+            img = self._generate_curve(draw, random.uniform(0.3, 1), random.randint(100, 900))
+            x = 0
+            y = random.randint(-self._height//2, self._height//2)
+        elif mode == 'normal':
+            img = self._generate_curve(draw, random.uniform(1, 10), random.randint(400, 900))
+            x = random.randint(-50, 0)
+            y = random.randint(-500, 500)
+        elif mode == 'heavy':
+            img = self._generate_curve(draw, random.uniform(7, 8), random.randint(300, 500))
+            x = random.randint(-50, 0)
+            y = random.randint(-50, 150)
+        elif mode == 'flat':
+            img = self._generate_curve(draw, random.uniform(50, 90), random.randint(100, 400))
+            x = int(-2500 * random.random())
+            y = random.randint(-self._height//2, self._height//2)
+
+        w, h = img.size
+        image.paste(img, (x, y), mask=img)
+
+    def add_dot_noise(self, image, draw):
+        color = self.random_dot_color()
+        x1 = random.randint(0, self._width)
+        y1 = random.randint(0, self._height)
+        x2 = x1 + random.randint(1, 2)
+        y2 = y1 + random.randint(1, 2)
+        draw.ellipse(
+            [
+                x1, y1, x2, y2,
+            ],
+            width=1, fill=color
+        )
+
+    def create_captcha_image(self, chars):
+        if random.random() < 0.5:
+            image = self.noised_background(self._width, self._height)
+        else:
+            image = super().background(self._width, self._height)
+
+        draw = Draw(image)
+        #         gray char noise
+        for i in range(50):
+            self.add_char_noise(
+                image,
+                c=random.choice(self.chars),
+                factor=random.uniform(0.1, 0.7),
+                pos=(random.randint(0, self._width), random.randint(0, self._height)),
+                draw=draw,
+            )
+        self.add_line_noise(image, draw)
+        self.add_curve_noise(image, draw)
+
+        for i in range(70):
+            super().add_line_noise(image, draw)
+
+        y = random.randint(-2, 4)
+        base_x = random.randint(2, 4)
+        font = random.choice(self.truefonts)
+        r_arr = [20, 50, 80, 100, 110]
+        g_arr = [50, 50, 20, 20, 20]
+        b_arr = [196, 150, 120, 70, 50]
+        for i, c in enumerate(chars):
+            r = int(min(max(0, r_arr[i] + random.normalvariate(10, 15)), 255))
+            g = int(min(max(0, g_arr[i] + random.normalvariate(10, 15)), 255))
+            b = int(min(max(0, b_arr[i] + random.normalvariate(10, 15)), 255))
+            img = super()._generate_char(c, color=(r, g, b), draw=draw, font=font)
+
+            w, h = img.size
+            x = base_x + i * 23 + random.randint(-2, 2)
+            image.paste(img, (x, y), mask=img)  # alpha = 0
+
+        for i in range(200):
+            self.add_dot_noise(image, draw)
+
+        for i in range(20):
+            super().add_line_noise(image, draw)
+        # image = image.filter(ImageFilter.GaussianBlur(random.uniform(0, 0.5)))
+        # image = image.resize((203, 66), PIL.Image.BICUBIC)
+        image = image.resize((self._width, self._height), PIL.Image.BICUBIC)
+        if random.random() < 0.5:
+            image = image.filter(ImageFilter.SMOOTH)
+        else:
+            image = image.filter(ImageFilter.SMOOTH_MORE)
+        image = image.resize((self._width * 4, self._height * 4), PIL.Image.BICUBIC)
+        if random.random() < 0.5:
+            image = image.filter(ImageFilter.SMOOTH)
         return image
--- src/generate_data.py
+++ src/generate_data.py
@@ -1,27 +1,34 @@
 # -*- coding: utf-8 -*-
-from captcha import SimpleCaptcha
+import captcha
 from tqdm import tqdm
 import argparse
 import os
 from os.path import join
 import random
+import matplotlib.pyplot as plt
+
 
 VALID_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
-def output_random_image(n: int, fonts: list, length: int, output_dir: str, invalid_set: set):
+def output_random_image(n: int, fonts: list, length: int, output_dir: str, invalid_set: set, verbose: bool):
     global VALID_CHARS
-    image = SimpleCaptcha(fonts=fonts)
+    image = captcha.HardCaptcha(fonts=fonts)
     for i in tqdm(range(n)):
         label = ''.join(random.choices(VALID_CHARS, k=length))
         while label in invalid_set:
             label = ''.join(random.choices(VALID_CHARS, k=length))
         invalid_set.add(label)
-        image.write(
+        im = image.write(
             label,
             os.path.join(output_dir, f'{label}.gif'),
             blur=False,
+            verbose=verbose,
         )
+        if verbose:
+            plt.imshow(im)
+            plt.show()
+            break
 
 
 if __name__ == '__main__':
@@ -29,6 +36,7 @@ if __name__ == '__main__':
     parser.add_argument('n', type=int)
     parser.add_argument('-l', '--length', type=int, default=5)
     parser.add_argument('-f', '--font_dir', type=str, default='../fonts/')
+    parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument(
         '-o', '--output',
         default='./outputs',
@@ -40,4 +48,4 @@ if __name__ == '__main__':
     fonts = [join(font_dir, f) for f in os.listdir(font_dir) if os.path.isfile(join(font_dir, f))]
     if not os.path.isdir(args.output):
         os.makedirs(args.output)
-    output_random_image(args.n, fonts, args.length, args.output, set())
+    output_random_image(args.n, fonts, args.length, args.output, set(), args.verbose)
